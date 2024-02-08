266eb59ff4c0d78615581b8b5b36252950906464
Shashwat Chouhan <ishashwat@tutanota.com>
2024-02-08 11:46:02 +0530

    create database

--- typescript/grafbase.config.ts
+++ typescript/grafbase.config.ts
@@ -1,30 +1,40 @@
-import { g, config } from '@grafbase/sdk'
+import { graph, config, connector} from '@grafbase/sdk'
 
-// Welcome to Grafbase!
-//
-// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+const g = graph.Standalone()
 
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
-  githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
+const mongodb = connector.MongoDB('MongoDB', {
+  url: 'https://data.mongodb-api.com/app/data-asdf/endpoint/data/v1',
+  apiKey: 'SECRET',
+  dataSource: 'myDatasource',
+  database: 'blogPlatform',
 })
 
-// @ts-ignore
-const Project = g.model('Project', {
-  title: g.string().length({ min: 3 }),
-  description: g.string(), 
-  image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
-})
+g.datasource(mongodb)
+
+mongodb
+  .model('User', {
+    name: g.string().length({ min: 2, max: 100 }),
+    email: g.string().unique(),
+    avatarUrl: g.url(),
+    description: g.string().length({ min: 2, max: 1000 }).optional(),
+    githubUrl: g.url().optional(),
+    linkedinUrl: g.url().optional(), 
+    
+  })
+  .collection('users')
+
+  mongodb
+  .model('Project', {
+    title: g.string().length({ min: 3 }),
+    description: g.string(), 
+    image: g.url(),
+    liveSiteUrl: g.url(), 
+    githubUrl: g.url(), 
+    
+  })
+  .collection('users')
+
 
 export default config({
-  schema: g,
+  graph: g,
 })
